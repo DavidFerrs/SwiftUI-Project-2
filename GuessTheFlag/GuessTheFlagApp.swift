c66c1fb8e4475871c54003f0ef7892298846717e
David Ferreira <81036027+DavidFerrs@users.noreply.github.com>
2024-10-27 15:36:24 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by David Ferreira on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
